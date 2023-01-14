1163e3df26eb177643466e1e636aac93bcf48ac2
pierrequiroul <pierrelac@hotmail.be>
2023-01-14 13:56:10 +0100

    Update family.js

--- src/commands/family/family.js
+++ src/commands/family/family.js
@@ -11,33 +11,65 @@ module.exports = async (client, interaction, args) => {
     let temp2 = [];
 
     for (let i = 0; i < data.Parent.length; i++) {
-        temp.push("<@!" + data.Children[i] + ">");
+        temp.push("<@!" + data.Parent[i] + ">");
     }
 
     for (let i = 0; i < data.Children.length; i++) {
         temp2.push("<@!" + data.Children[i] + ">");
     }
     
-    client.embed({
-        title: `👪・Famille de ${target.username}`,
-        thumbnail: target.avatarURL({ size: 1024 }),
-        fields: [
-            {
+    let fields = [{
                 name: `Partenaire`,
                 value: `${data && data.Partner ? `<@!${data.Partner}>` : `Cette personne n'est pas mariée`}`
             },
             {
                 name: `Parents`,
                 value: `${data && data.Parent.length > 0 ? `${temp.join(", ")}` : `Cette personne n'a pas de parents`}`
-            },
-            /*{
+            }
+        ];
+
+        // Check siblings
+        /*if (data && data.Parent.length > 0) {
+            let temp3 = [];
+            for (let i = 0; i < data.Parent.length; i++) {
+                const dataParent = await Schema.findOne({ Guild: interaction.guild.id, User: data.Parent[i] });
+                for (let j = 0; j < dataParent.Children.length; j++) {
+                    temp3.push("<@!" + dataParent.Children[i] + ">");
+                };
+                temp3[j].concat("\n");
+            };
+            fields.push({
                 name: `Frères/Soeurs`,
-                value: `${data && data.Children.length > 0 ? `${data.Children.join(", ")}` : `Cette personne n'a pas d'enfants`}`
-            },*/
-            {
+                value: `${temp3.join(", ")}`
+            });
+        };*/
+        
+        if (data && data.Parent.length > 0) {
+            let temp3 = [];
+            const parentPromises = data.Parent.map(async parent => {
+                const dataParent = await Schema.findOne({ Guild: interaction.guild.id, User: parent });
+                if (dataParent && dataParent.Children.length > 0) {
+                    temp3.push(...dataParent.Children.map(child => "<@!" + child + ">"));
+                }
+            });
+            await Promise.all(parentPromises);
+            if(data.Parent.length > 1) {
+                temp3[temp3.length - 1] = temp3[temp3.length - 1] + '\n';
+            }
+            fields.push({
+                name: `Frères/Soeurs`,
+                value: `${temp3.join(", ")}`
+            });
+        }
+
+        fields.push({
                 name: `Enfants`,
                 value: `${data && data.Children.length > 0 ? `${temp2.join(", ")}` : `Cette personne n'a pas d'enfants`}`
-            }
+            });
+    client.embed({
+        title: `👪・Famille de ${target.username}`,
+        thumbnail: target.avatarURL({ size: 1024 }),
+        fields: fields;
         ],
         type: 'editreply'
     }, interaction, false)
