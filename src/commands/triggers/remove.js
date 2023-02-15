ae757fd340ad6d0c09c2a13f69b2893d3d5217a0
pierrequiroul <pierrelac@hotmail.be>
2023-02-15 23:16:48 +0100

    Update remove.js

--- src/commands/triggers/remove.js
+++ src/commands/triggers/remove.js
@@ -1,35 +1,36 @@
 const Discord = require('discord.js');
 
-const Schema = require("../../database/models/blacklist");
+const Schema = require("../../database/models/triggers");
 const { blacklistedWords } = require("../../Collection");
 
 module.exports = async (client, interaction, args) => {
-    const word = interaction.options.getString('word');
+    const word = interaction.options.getString('nom');
 
-    Schema.findOne({ Guild: interaction.guild.id }, async (err, data) => {
+    Schema.findOne({ Guild: interaction.guild.id, Trigger: nom }, async (err, data) => {
         if (data) {
-            if (!data.Words.includes(word)) {
+            if (!data.Trigger == nom) {
                 return client.errNormal({
                     error: `Ce mot n'existe pas dans la base de données !`,
                     type: 'editreply'
                 }, interaction);
             }
 
-            const filtered = data.Words.filter((target) => target !== word);
-
-            await Schema.findOneAndUpdate({ Guild: interaction.guild.id }, {
+            //const filtered = data.Words.filter((target) => target !== word);
+            
+            await Schema.findOneAndDelete({Guild: interaction.guild.id, Trigger: nom });
+            /*await Schema.findOneAndUpdate({  }, {
                 Guild: interaction.guild.id,
                 Words: filtered
-            });
+            });*/
 
-            blacklistedWords.set(interaction.guild.id, filtered)
+            triggersWords.set(interaction.guild.id, filtered)
 
             client.succNormal({
                 text: `Ce mot a été retiré de la liste noire !`,
                 fields: [
                     {
                         name: `<:uo_BotEvent:1015565719330627584> ┆ Mot`,
-                        value: `${word}`
+                        value: `${nom}`
                     }
                 ],
                 type: 'editreply'
