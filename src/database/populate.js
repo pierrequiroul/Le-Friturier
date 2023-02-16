b9322b3404aa503f81794b8582ed8baf2d42125f
pierrequiroul <pierrelac@hotmail.be>
2023-02-17 00:01:38 +0100

    Update populate.js

--- src/database/populate.js
+++ src/database/populate.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 
 async function populate() {
-    const trigWord = mongoose.getCollection("triggers-words");
+    const trigWord = mongoose.collection("triggers-words");
     
     
     console.log(chalk.blue(trigWord[0].Trigger));
