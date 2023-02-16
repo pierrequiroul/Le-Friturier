4f4d635f1bc60367828aeaf139186720051b00a0
pierrequiroul <pierrelac@hotmail.be>
2023-02-16 23:43:31 +0100

    Update populate.js

--- src/database/populate.js
+++ src/database/populate.js
@@ -5,7 +5,7 @@ async function populate() {
     const trigWord = mongoose.getCollection("triggers-words");
     
     
-    console.log(chalk.blue(trigWord[0].Trigger)
+    console.log(chalk.blue(trigWord[0].Trigger))
     
     return;
 }
