181b6cdc2eef2851a6d5e5a502be1283d70e828e
pierrequiroul <pierrelac@hotmail.be>
2023-02-18 12:42:31 +0100

    Update connect.js

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const chalk = require('chalk');
 
 async function connect() {
@@ -17,9 +17,34 @@ async function connect() {
             }
         });
     });
-    
-    return data;
+}
+
+module.exports.connect = connect;*/
+
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGO_TOKEN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const collection = mongoose.connection.db.collection("triggers-words");
+        return new Promise((resolve, reject) => {
+            collection.find({}).toArray(function(err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    } catch (err) {
+        console.error(chalk.red.bold("Failed to connect to MongoDB:"));
+        console.error(err);
+        process.exit(1);
+    }
 }
 
 module.exports.connect = connect;
-module.exports.triggerWords = connect();
